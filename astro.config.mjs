f1126de478a78d7e9e3c103a6879d70aa2d16646
Lee Palacios <vitualizz@gmail.com>
2024-04-28 04:44:35 -0500

    feat: IMplement correctly i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,10 @@ import playformCompress from '@playform/compress'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), playformCompress()]
+  integrations: [tailwind(), playformCompress()],
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es', 'en']
+  }
 })
 
--- src/i18n/locale/es.ts
+++ src/i18n/locale/es.ts
@@ -2,18 +2,31 @@ import type { LanguageLocale } from '../ui'
 
 const locale: LanguageLocale = {
   nav: {
-    presentation: 'Presentación',
-    about_me: 'Sobre mi',
-    projects: 'Proyectos',
-    contact_me: 'Contacto',
+    presentation: "Presentación",
+    about_me: "Sobre mí",
+    projects: "Proyectos",
+    contact_me: "Contáctame"
   },
   presentation: {
-    description: "<span class='text-color-1'>+7 años</span> de experiencia en programación, ¡soy tu aliado amante de la tecnología listo para revolucionar el mundo digital!"
+    description: "<span class='text-color-1'>+7 años</span> de experiencia en programación, ¡soy tu aliado amante de la tecnología listo para sacudir tu mundo digital!"
   },
   about_me: {
-    section_about_me: 'Sobre mi',
-    section_skills: 'Habilidades',
-    biography: "Soy un <span class='text-color-2'>ingeniero de software</span>, sumergiéndome en un emocionante <span class='text-color-2'>universo tecnológico</span> donde cada línea de código es un trazo de creatividad. Mi corazón late al ritmo de <span class='text-color-1'>Javascript</span> y <span class='text-color-1'>Ruby</span>, pero mi curiosidad me ha llevado a explorar un arcoíris de lenguajes. En cada proyecto, busco la <span class='text-color-2'>excelencia</span> y la <span class='text-color-2'>calidad</span>, convirtiendo el proceso de codificación en una <span class='text-color-2'>aventura emocionante</span> y <span class='text-color-2'>gratificante</span>."
+    section_about_me: "Acerca de mí",
+    section_skills: "Habilidades",
+    biography: "Soy un <span class='text-color-2'>ingeniero de software</span>, adentrándome en un emocionante universo <span class='text-color-2'>tecnológico</span> donde cada línea de código es un trazo de creatividad. Mi corazón late al ritmo de <span class='text-color-1'>Javascript</span> y <span class='text-color-1'>Ruby</span>, pero mi curiosidad me ha llevado a explorar un arcoíris de lenguajes. En cada proyecto, busco la excelencia y <span class='text-color-2'>calidad</span>, convirtiendo el proceso de codificación en una aventura <span class='text-color-2'>emocionante</span> y <span class='text-color-2'>gratificante</span>.",
+    skills_section_frameworks: "Frameworks y Bibliotecas",
+    skills_section_others: "Otros",
+    skills_section_languages: "Lenguajes"
+  },
+  projects: {
+    section_projects: "Proyectos"
+  },
+  contact_me: {
+    tell_me: "Hablemos</br>Cuéntame sobre tu Proyecto",
+    send_message_me: "Envíanos un mensaje ✨"
+  },
+  footer: {
+    thanks: "Con amor @vitualizz"
   }
 }
 
