3ecc49b3f7e9ed9c68eb5eb0097dc00cddcdc2dd
Lee Palacios <vitualizz@gmail.com>
2024-04-28 03:15:55 -0500

    🔧 (astro.config.mjs): Add playformCompress integration to astro config ⬆️ (package.json): Upgrade @playform/compress to version 0.0.3 📝 (components): Update styles in AboutMeSection, Navbar, PresentationSection, ProjectCard, ThemeToggleBtn 📝 (projects.json): Update project descriptions in English and Spanish, adjust stack icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 
+import playformCompress from '@playform/compress'
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), playformCompress()]
 })
+
