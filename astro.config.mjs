998fa53559d96536f5bcd4d31b46e7ea15505e8d
Lee Palacios <vitualizz@gmail.com>
2025-07-02 13:38:42 -0500

    feat(astro.config.mjs): Add site URL and update integration to include Playform Compress for improved performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import playformCompress from '@playform/compress'
 
 // https://astro.build/config
 export default defineConfig({
+  sitre: 'https://vitualizz.vercel.app',
   integrations: [tailwind(), playformCompress()],
   i18n: {
     defaultLocale: 'es',
